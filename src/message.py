bd10e1cbec922b9625fb094ac3e723168fe1c4ba
Daniel Miranda <danielvcmiranda@gmail.com>
2021-02-24 16:17:11 -0300

    classe FileMessage e refatoracao

--- src/message.py
+++ src/message.py
@@ -33,7 +33,7 @@ class ConnectionMessage(Message):
             udp_port_bytes = self.udp_port.to_bytes(4,BYTE_ORDER)
         except OverflowError:
             raise Exception('udp_port length is too big')
-        message = Message.message_types['CONNECTION']+udp_port_bytes
+        message = Message.message_types[self.type]+udp_port_bytes
         return message
     
     def decode(message):
@@ -68,7 +68,7 @@ class InfoFileMessage(Message):
         file_size_bytes = self.convert_file_size_to_bytes()
         file_name = self.file_name.encode(ENCODING)
         file_name += b' '*(InfoFileMessage.max_file_name_len-len(file_name))
-        message = Message.message_types['INFO FILE'] + file_name + file_size_bytes
+        message = Message.message_types[self.type] + file_name + file_size_bytes
         return message
     
     def decode(message):
@@ -79,12 +79,52 @@ class InfoFileMessage(Message):
         return InfoFileMessage(file_name, file_size)
 
 class FileMessage(Message):
+    max_serial_number_len = 4
+    max_payload_size_len = 8
+
     def __init__(self, serial_number, payload_size, payload):
         super().__init__('FILE')
-        self.serial_number = str(serial_number)
-        self.payload_size = str(payload_size)
+        self.serial_number = int(serial_number)
+        self.payload_size = int(payload_size)
         self.payload = payload
     
     def encode(self):
-        if len(self.serial_number) > 4:
-            raise Exception('Serial')
+        serial_number_bytes = self.convert_serial_number_to_bytes()
+        payload_size_bytes = self.convert_payload_size_to_bytes()
+        payload = self.get_right_size_payload()
+        
+        message = Message.message_types[self.type] + serial_number_bytes + payload_size_bytes + payload
+        
+        return message
+
+    def decode(message: bytes):
+        serial_number_start_idx = comum.SIMPLE_MESSAGE_SIZE
+        payload_size_start_idx = FileMessage.max_serial_number_len+serial_number_start_idx
+        payload_start_idx = payload_size_start_idx+FileMessage.max_payload_size_len
+        serial_number = int.from_bytes(message[serial_number_start_idx:payload_size_start_idx],BYTE_ORDER)
+        payload_size = int.from_bytes(message[payload_size_start_idx:payload_start_idx],BYTE_ORDER)
+        payload = message[payload_start_idx:payload_size]
+        return FileMessage(serial_number, payload_size, payload)
+
+    def convert_serial_number_to_bytes(self):
+        try:
+            serial_number_bytes = self.serial_number.to_bytes(self.max_serial_number_len, BYTE_ORDER)
+            return serial_number_bytes
+        except OverflowError:
+            raise Exception(f'Serial number {self.serial_number} is too long')
+    
+    def convert_payload_size_to_bytes(self):
+        try:
+            payload_size_bytes = self.payload_size.to_bytes(self.max_payload_size_len, BYTE_ORDER)
+            return payload_size_bytes
+        except OverflowError:
+            raise Exception(f'Payload size {self.payload_size} is too long')
+    
+    def get_right_size_payload(self):
+        payload = self.payload
+        if len(self.payload)< comum.MAX_FILE_PART_SIZE:
+            payload = self.payload + b' '*(comum.MAX_FILE_PART_SIZE-len(self.payload))
+        return payload
+
+
+
--- src/message_factory.py
+++ src/message_factory.py
@@ -1,44 +1,51 @@
-from .message import Message, ConnectionMessage, InfoFileMessage
-
+from .message import Message, ConnectionMessage, InfoFileMessage, FileMessage
+from . import comum
 class MessageFactory:
     def build(message_type, **kwargs):
         if message_type not in Message.message_types:
             raise Exception(f"Message type should be one of {[key for key in Message.message_types]}")
     
-        if message_type == 'CONNECTION':
-            if 'udp_port' not in kwargs:
-                raise Exception('udp_port must be passed as an argument for this kind of message')
-            udp_port = str(kwargs['udp_port'])
+        elif message_type == 'CONNECTION':
+            udp_port = str(MessageFactory.get_arg('udp_port',kwargs))
             message = ConnectionMessage(udp_port)
             return message.encode()
         
-        if message_type == 'INFO FILE':
-            if 'file_name' not in kwargs:
-                raise Exception('file_name must be passed as an argument for this kind of message')
-            file_name = str(kwargs['file_name'])
-            if 'file_size' not in kwargs:
-                raise Exception('file_size must be passed as an argument for this kind of message')
-            file_size = str(kwargs['file_size'])
+        elif message_type == 'INFO FILE':
+           
+            file_name = str(MessageFactory.get_arg('file_name', kwargs))
+            file_size = str(MessageFactory.get_arg('file_size', kwargs))
+
             message = InfoFileMessage(file_name, file_size)
+            return message.encode()
+        
+        elif message_type == 'FILE':
+            serial_number = int(MessageFactory.get_arg('serial_number', kwargs))
+            payload_size = int(MessageFactory.get_arg('payload_size', kwargs))
+            payload = MessageFactory.get_arg('payload', kwargs)
+            message = FileMessage(serial_number, payload_size, payload)
             return message.encode()
         
         else: #mensagens simples
             return Message(message_type).encode()
 
     def decode(message: bytes):
-        type_header = message[0:2]
-
-        if type_header == Message.message_types["CONNECTION"]:
-            try:
+        type_header = message[0:comum.SIMPLE_MESSAGE_SIZE]
+        try:
+            if type_header == Message.message_types["CONNECTION"]:
                 return ConnectionMessage.decode(message)
-            except:
-                return Message(None)
-        
-        elif type_header == Message.message_types["INFO FILE"]:
-            try:
-                return InfoFileMessage.decode(message)
-            except:
-                return Message(None)
             
-        else:
-            return Message.decode(message)
+            elif type_header == Message.message_types["INFO FILE"]:
+                return InfoFileMessage.decode(message)
+            
+            elif type_header == Message.message_types['FILE']:
+                return FileMessage.decode(message)
+            
+            else:
+                return Message.decode(message)
+        except:
+            return Message(None)
+
+    def get_arg(arg_name, kwargs):
+        if arg_name not in kwargs:
+            raise Exception(f'{arg_name} must be passed as an argument for this kind of message')
+        return kwargs[arg_name]
